d1a7b2e9684f566a4983a98c91402a9ecce364e6
momena <momena.salloum59@gmail.com>
2023-11-11 20:26:23 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const whereIsWaldo = (arr) => {
-
     for (let row = 0; row < arr.length; row++) {
-        for (let col = 0; col < arr[row].length - 1; col++) {
-            if (arr[row][col] != arr[row][col + 1]) {
-                // console.log(arr[i]);
-                return `[${row + 1}, ${col + 1}]`;
+        for (let col = 0; col < arr[row].length; col++) {
+            if (arr[row][col] !== arr[0][0]) {
+                return [row + 1, col + 1];
             }
         }
     }
     return "No Waldo found";
 }
 console.log(whereIsWaldo([
+    ["O", "O", "P", "O"],
     ["O", "O", "O", "O"],
     ["O", "O", "O", "O"],
     ["O", "O", "O", "O"],
     ["O", "O", "O", "O"],
-    ["P", "O", "O", "O"],
     ["O", "O", "O", "O"]
 ]));
 console.log(whereIsWaldo([
